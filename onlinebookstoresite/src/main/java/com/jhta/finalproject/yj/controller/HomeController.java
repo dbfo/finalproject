ad96e3b2db85301a9e446d0d2ca96f60376a23d2
Chloe <duswn4627@hanmail.net>
2020-07-02 15:55:56 +0900

    0702-tiles

---
+++ onlinebookstoresite/src/main/java/com/jhta/finalproject/yj/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.jhta.finalproject.yj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return ".main";
+	}
+}
