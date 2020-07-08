3e0b7ae4a50cc0b5967137e8b4eaf8bf070d8150
heedo.lee <luj0605@gmail.com>
2020-07-08 11:36:10 +0900

    0708

--- onlinebookstoresite/src/main/java/com/jhta/finalproject/hd/controller/HomeController.java
+++ onlinebookstoresite/src/main/java/com/jhta/finalproject/hd/controller/HomeController.java
@@ -1,23 +1,10 @@
 package com.jhta.finalproject.hd.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.jhta.finalproject.hd.service.HomeService;
 
 /**
  * 홈에서 보내줘야하는것.
@@ -25,9 +12,6 @@ import com.jhta.finalproject.hd.service.HomeService;
  */
 @Controller
 public class HomeController { 
-	@Autowired
-	private HomeService service;
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpSession session,HttpServletRequest req) {
 		session.setAttribute("cp", req.getContextPath());
